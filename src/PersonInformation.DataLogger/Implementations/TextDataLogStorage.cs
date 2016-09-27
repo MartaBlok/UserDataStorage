3ab68053a09b6ed5cc87e46f05376e1374a57618
Marta Blok <marta.blok@gmail.com>
2016-09-27 10:07:47 +0200

    NLog configuration

--- src/PersonInformation.DataLogger/Implementations/TextDataLogStorage.cs
+++ src/PersonInformation.DataLogger/Implementations/TextDataLogStorage.cs
@@ -6,16 +6,38 @@ using System.Threading.Tasks;
 using PersonInformation.DataLogger.Interfaces;
 using PersonInformation.DataLogger.Models;
 using NLog;
+using NLog.Config;
+using NLog.Targets;
 
 namespace PersonInformation.DataLogger.Implementations
 {
     public class TextDataLogStorage : IUserDataLogStorage
     {
-        private static Logger logger = LogManager.GetCurrentClassLogger();
+        private static Logger _logger = LogManager.GetCurrentClassLogger();
 
         public void Log(UserDataLogDTO dto)
         {
-            logger.Log(LogLevel.Info, $"");
+            // Step 1. Create configuration object 
+            var config = new LoggingConfiguration();
+
+            // Step 2. Create targets and add them to the configuration 
+            var fileTarget = new FileTarget();
+            config.AddTarget("file", fileTarget);
+
+            // Step 3. Set target properties 
+            fileTarget.FileName = "${basedir}/file.txt";
+            fileTarget.Layout = "${message}";
+
+            // Step 4. Define rules
+            var rule = new LoggingRule("*", LogLevel.Info, fileTarget);
+            config.LoggingRules.Add(rule);
+
+            // Step 5. Activate the configuration
+            LogManager.Configuration = config;
+            
+            _logger = LogManager.GetLogger("TextDataLogStorage");
+            _logger.Info($"{dto.Name}, {dto.Surname}");
+            
         }
     }
 }
